75c4ed34856e6345659cb03e9f048aec812894a0
Peace Aisosa Osarenren <105794651+LowCodeDataGirl@users.noreply.github.com>
2023-07-12 02:01:40 -0700

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,8 +1,8 @@
 import json
 import pandas as pd
 
-# Specify the path of your json file
-json_file = r"C:\Users\Peace Paix\Documents\Web Scraping\Amazon Web Scraping\Pages Snapshots\all_data.json"
+# Specify the path of your JSON file
+json_file = r"C:\Users\Peace\Path\data.json"
 
 # Load the json file
 with open(json_file) as f:
@@ -12,4 +12,4 @@ with open(json_file) as f:
 df = pd.DataFrame(data)
 
 # Write the data to a CSV file
-df.to_csv(r"C:\Users\Peace Paix\Documents\Web Scraping\all_data.csv", index=False)
+df.to_csv(r"C:\Users\Peace\data.csv", index=False)
